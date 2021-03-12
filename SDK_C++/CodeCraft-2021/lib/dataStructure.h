e4d1a2c9d362bdec31da55eb008eed2009c3a036
root <zhiguojiangelvis@15200947573@qq.com>
2021-03-12 20:47:29 +0800

    基于容量和价格的贪心策略，即优先使用剩余容量最小且满足要求的服务器，买的时候也是挑最便宜的满足要求的买，代码目前还无法通过编译

--- SDK_C++/CodeCraft-2021/lib/dataStructure.h
+++ SDK_C++/CodeCraft-2021/lib/dataStructure.h
@@ -3,9 +3,14 @@
 using namespace std;
 class server{
 public:
+    string serverType;
     int coreNum[2], ram[2], costBuy, costDay;
-    server(int _coreNum, int _ram, int _costBuy, int _costDay){
-        coreNum[0]=coreNum[1]=_coreNum; ram[0]=ram[1]=_ram; costBuy=_costBuy; costDay=_costDay;
+    server(string _serverType, int _coreNum, int _ram, int _costBuy, int _costDay){
+        serverType=_serverType;coreNum[0]=coreNum[1]=_coreNum; ram[0]=ram[1]=_ram; costBuy=_costBuy; costDay=_costDay;
+    }
+    bool operator<(const server other){//重载<运算符
+        if(this->costBuy!=other.costBuy)return this->costBuy<other.costBuy;
+        return this->costDay<other.costDay;
     }
 };
 
@@ -13,8 +18,9 @@ class VM{
 public:
     int coreNum, ram;
     bool doubleNode;
-    VM(int _coreNum, int _ram, bool _doubleNode){
-        coreNum=_coreNum; ram=_ram; doubleNode=_doubleNode;
+    string VMType;
+    VM(string _VMType, int _coreNum, int _ram, bool _doubleNode){
+        VMType=_VMType;coreNum=_coreNum; ram=_ram; doubleNode=_doubleNode;
     }
 };
 
@@ -24,5 +30,30 @@ public:
     string VMType,VMID;
     request(bool _reType, string _VMType, string _VMID){
         reType=_reType; VMType=_VMType; VMID=_VMID;
+    }
+};
+
+class serverNode{
+public:
+    string serverType;
+    int ID;
+    bool A;
+    int powerCost;
+    serverNode(string _serverType, int _ID,bool _A,int _powerCost){
+        serverType=_serverType;ID=_ID;A=_A;powerCost=_powerCost;
+    }
+    bool operator<(const serverNode other){//重载<运算符
+        if(this->ID==other.ID)return false;
+        return this->powerCost<other.powerCost;
+    }
+};
+
+class VMNode{
+    int hostID;
+    bool A,B;
+    VMNode(int _hostID, bool _A, bool _B){
+        hostID=_hostID;
+        A=_A;
+        B=_B;
     }
 };
--- SDK_C++/CodeCraft-2021/lib/run.h
+++ SDK_C++/CodeCraft-2021/lib/run.h
@@ -1,44 +1,260 @@
 #include "dataStructure.h"
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <time.h>
 #include <algorithm>
 #include <assert.h>
+#include  <map>
 using namespace std;
 class run{
 private:
     string filePath;
     //服务器和虚拟机的所有型号
-    unordered_map< string, server* > serverWarehouse;
+    //先按核数排序然后按照内存排序，从小到大
+    map<int, map<int,set<server> > > serverWarehouse;
+    map<int, map<int, set<serverNode> > > serverPossessed;
+    vector<int*> leftSource;//4个数记录每个服务器的核数和内存的使用情况
     unordered_map< string, VM* > VMWarehouse;
-    vector<server*> serverPossessed;
-    unordered_map<int, string > VMPossessed;
-    vector<request*> requestWarehouse;
+    unordered_map<int, VMNode* > VMPossessed;
+    vector<request*> *requestWarehouse;
+
+    //每一次的决策
+    unordered_map<string,int> buyServer;
+    vector<string> allocateVM;
+    vector<string> mvVM;
+    int moveTime;
+    int moveMax;
+
     int days;
+    int serverNum;
+    int VMNum;
 public:
     run(string filePath){
+        VMNum=0;
+        serverNum=0;
         this->filePath=filePath;
         loadData();
         for(int i=0;i<days;i++)processRequests(i);
     }
     ~run(){
-        for(unordered_map<string, server*>::iterator it=serverWarehouse.begin();it!=serverWarehouse.end();it++)delete it->second;
         for(unordered_map<string, VM*>::iterator it=VMWarehouse.begin();it!=VMWarehouse.end();it++)delete it->second;
-        for(server *tmp:serverPossessed)delete tmp;
-        for(request *tmp:requestWarehouse)delete tmp;
+        for(int *tmp:leftSource)delete[] tmp;
+        for(unordered_map<int, VMNode* >::iterator it=VMPossessed.begin();it!=VMPossessed.end();it++)delete it->second;
     }
     void loadData();
     void processRequests(int dayth);
     void generateServer(string &serverType,string &cpuCores,string &memorySize,string &serverCost,string &powerCost);
     void generateVM(string &vmType,string &vmCpuCores,string &vmMemory,string &vmTwoNodes);
-    void generateRequest(string &op, string &reqId);
-    void generateRequest(string &op, string &reqVmType, string &reqId);
+    void generateRequest(string &op, string &reqId, int today);
+    void generateRequest(string &op, string &reqVmType, string &reqId, int today);
+    void moveAction();
+    void printStrategy();
+    //返回购买型号
+    string buy(int coreNeed,int ramNeed);
 };
+
+//输出一天的策略
+void run::printStrategy(){
+    //买服务器策略
+    string buyAllStr="(purchase, ";
+    buystr+=to_string(buyServer.size())+")";
+    cout<<buystr<<endl;;
+    for(auto it=buyServer.begin();it!=buyServer.end();it++){
+        string buySingleStr="(";
+        buySingleStr+=(it->first+", "+to_string(it->second)+")");
+        cout<<buySingleStr<<endl;
+    }
+    //迁移虚拟机策略
+    for(string str: mvVM)cout<<mvVM<<endl;;
+    //分配服务器
+    for(string str:allocateVM)cout<<str<<endl;
+}
+
+void run::moveAction(){
+    mvVM.push_back("(migration, 0)");
+}
+
+string run::buy(int coreNeed,int ramNeed){//买最便宜的
+    set<server> satisfied=serverWarehouse.lower_bound(coreNeed)->second.lower_bound(ramNeed)->second;
+    return (*satisfied.begin()).serverType;
+}
 
 //处理第day天的请求
 void run::processRequests(int dayth){
+    buyServer.clear();
+    allocateVM.clear();
+    mvVM.clear();
+    moveTime=0;
+    moveMax=VMNum/200;
+    unsigned int buyCost=0,powerCost=0;
+    for(request *req:requestWarehouse[dayth]){
+        //给请求分配机器
+        int VMID=req->VMID;
+        VM *vm=VMWarehouse[req->VMType];
+        int coreNeed=vm->coreNum,ramNeed=vm->ram;
+        if(req->reType){
+            VMNum++;
+            bool allocated=false;
+            if(vm->doubleNode){//处理双节点请求
+               map<int, map<int, set<serverNode> > >::iterator it=serverPossessed.lower_bound(coreNeed);
+               while(it!=serverPossessed.end()&&!allocated){
+                    map<int, set<serverNode*> >::iterator itin=it->second.lower_bound(ramNeed);
+                    if(itin!=it->second.end()){
+                        set<serverNode>::iterator posi=itin->second.begin();
+                        while(posi!=itin->second.end()){
+                            serverNode tmp=*posi;
+                            int ID=tmp.ID;
+                            if(leftSource[ID][0]>=coreNeed&&leftSource[ID][1]>=ramNeed&&leftSource[ID][2]>=coreNeed&&leftSource[ID][3]>=ramNeed){
+                                allocated=true;
+                                allocateVM.push_back("("+to_string(tmp.ID)+")");
+                                VMPossessed[VMID]=new VMNode(tmp.ID,true,true);
+                                if(tmp.A){
+                                    serverPossessed[leftSource[ID][0]][leftSource[ID][1]].erase(posi);
+                                    leftSource[ID][0]-=coreNeed;leftCore[ID][1]-=ramNeed;
+                                    serverPossessed[leftSource[ID][0]][leftSource[ID][1]].insert(tmp);
+                                    set<serverNode*>::iterator del;
+                                    for(set<serverNode*>::iterator other=serverPossessed[leftSource[ID][2]][leftSource[ID][3]].begin();
+                                    other!=serverPossessed[leftSource[ID][2]][leftSource[ID][3]].end();other++){
+                                        if(other->ID==ID&&!other->A){
+                                            del=other;
+                                            break;
+                                        }
+                                    }
+                                    serverPossessed[leftSource[ID][2]][leftSource[ID][3]].erase(del);
+                                    leftSource[ID][2]-=coreNeed;leftCore[ID][3]-=ramNeed;
+                                    serverPossessed[leftSource[ID][2]][leftSource[ID][3]].insert(*del);
+                                }
+                                else{
+                                    serverPossessed[leftSource[ID][2]][leftSource[ID][3]].erase(posi);
+                                    leftSource[ID][2]-=coreNeed;leftCore[ID][3]-=ramNeed;
+                                    serverPossessed[leftSource[ID][2]][leftSource[ID][3]].insert(tmp);
+                                    set<serverNode*>::iterator del;
+                                    for(set<serverNode*>::iterator other=serverPossessed[leftSource[ID][0]][leftSource[ID][1]].begin();
+                                    other!=serverPossessed[leftSource[ID][0]][leftSource[ID][1]].end();other++){
+                                        if(other->ID==ID&&other->A){
+                                            del=other;
+                                            break;
+                                        }
+                                    }
+                                    serverPossessed[leftSource[ID][0]][leftSource[ID][1]].erase(del);
+                                    leftSource[ID][0]-=coreNeed;leftCore[ID][1]-=ramNeed;
+                                    serverPossessed[leftSource[ID][0]][leftSource[ID][1]].insert(*del);
+                                }
+                            }
+                            posi++;
+                        }
+                    }
+                    it++;
+               }
+            }
+            else{//处理单节点请求，找到满足要求的剩余容量最小的机器
+                map<int, map<int, set<serverNode> > >::iterator it=serverPossessed.lower_bound(coreNeed);
+                while(it!=serverPossessed.end()&&!allocated){
+                    map<int, set<serverNode*> >::iterator itin=it->second.lower_bound(ramNeed);
+                    if(itin!=it->second.end()){
+                        allocated=true;
+                        //找到满足条件的每日耗电量最小的
+                        serverNode tmp=*(itin->second.begin());
+                        string A="B";
+                        int coreDel=it->first, ramDel=itin->first,num=tmp.ID;
+                        if(tmp.A){
+                            A="A";
+                            leftSource[num][0]-=coreNeed;
+                            leftSource[num][1]-=ramNeed;
+                        }
+                        else{
+                            leftSource[num][2]-=coreNeed;
+                            leftSource[num][3]-=ramNeed;
+                        }
+                        allocateVM.push_back("("+to_string(tmp.ID)+", "+A+")");
+                        VMPossessed[VMID]=new VMNode(tmp.ID,(A=="A"),(A=="B"));
+                        serverPossessed[coreDel][ramDel].erase(serverPossessed[coreDel][ramDel].begin());
+                        coreDel-=coreNeed;ramDel-=ramNeed;
+                        serverPossessed[coreDel][ramDel].insert(tmp);
+                    }
+                    it++;
+                }
+            }
+            //买新机器
+            if(!allocated){
+                if(vm->doubleNode){
+                    coreNeed/=2;
+                    ramNeed/=2;
+                }
+                string buyType=buy(coreNeed,ramNeed);
+                server *newServer=&serverWarehouse[buyType];
+                buyServer[buyType]++;
+                if(vm->doubleNode){
+                    serverNode tmp1=serverNode(buyType,serverNum,true);
+                    serverNode tmp2=serverNode(buyType,serverNum,false);
+                    serverPossessed[newServer->coreNum-coreNeed][newServer->ram-ramNeed].insert(tmp1);
+                    serverPossessed[newServer->coreNum-coreNeed][newServer->ram-ramNeed].insert(tmp2);
+                    int *left=new int[4];left[0]=newServer->coreNum-coreNeed;left[1]=newServer->ram-ramNeed;
+                    left[2]=newServer->coreNum-coreNeed;left[3]=newServer->ram-ramNeed;
+                    leftSource.push_back(left);
+                    allocateVM.push_back("("+to_string(serverNum)+")");
+                    VMPossessed[VMID]=new VMNode(serverNum,true,true);
+                }
+                else{
+                    serverNode tmp1=serverNode(buyType,serverNum,true);
+                    serverNode tmp2=serverNode(buyType,serverNum,false);
+                    serverPossessed[newServer->coreNum-coreNeed][newServer->ram-ramNeed].insert(tmp1);
+                    serverPossessed[newServer->coreNum][newServer->ram].insert(tmp2);
+                    int *left=new int[4];left[0]=newServer->coreNum-coreNeed;left[1]=newServer->ram-ramNeed;
+                    left[2]=newServer->coreNum;left[3]=newServer->ram;
+                    leftSource.push_back(left);
+                    allocateVM.push_back("("+to_string(serverNum)+", A)"+);
+                    VMPossessed[VMID]=new VMNode(serverNum,true,false);
+                }
 
+                serverNum++;
+            }
+        }
+        else{//删除一个虚拟机
+            VMNum++;
+            //分配在哪一台主机上
+            VMNode *host=VMPossessed[VMID];
+            int hostID=host->hostID;
+            //归还资源
+            bool A=host->A,B=host->B;
+            int findCore,findRam;
+            if(A){
+                findCore=leftSource[hostID][0];
+                findRam=leftSource[hostID][1];
+                leftSource[hostID][0]+=coreNeed;
+                leftSource[hostID][1]+=ramNeed;
+                serverNode *add;
+                set<serverNode> goalSet=serverPossessed[findCore][findRam];
+                for(auto goal=goalSet.begin();goal!=goalSet.end();goal++){
+                    if(goal->ID==hostID&&goal->A){
+                            add=goal;
+                    }
+                }
+                serverPossessed[findCore][findRam].erase(add);
+                serverPossessed[leftSource[hostID][0]][leftSource[hostID][1]].insert(*add);
+            }
+            if(B){
+                findCore=leftSource[hostID][2];
+                findRam=leftSource[hostID][3];
+                leftSource[hostID][2]+=coreNeed;
+                leftSource[hostID][3]+=ramNeed;
+                serverNode *add;
+                set<serverNode> goalSet=serverPossessed[findCore][findRam];
+                for(auto goal=goalSet.begin();goal!=goalSet.end();goal++){
+                    if(goal->ID==hostID&&!goal->A){
+                            add=goal;
+                    }
+                }
+                serverPossessed[findCore][findRam].erase(add);
+                serverPossessed[leftSource[hostID][2]][leftSource[hostID][3]].insert(*add);
+            }
+        }
+        moveAction();
+        delete req;
+    }
+    printStrategy();
 }
 
 
@@ -61,7 +277,8 @@ void run::generateServer(string &serverType,string &cpuCores,string &memorySize,
     for(int i=0;i<powerCost.size()-1;i++){
         _powerCost = 10*_powerCost + powerCost[i] - '0';
     }
-    serverWarehouse[_serverType] = new server(_cpuCores/2,_memorySize/2,_serverCost,_powerCost);
+
+    serverWarehouse[_cpuCores/2][_memorySize/2].insert(server(_serverType, _cpuCores/2,_memorySize/2,_serverCost,_powerCost));
 }
 
 
@@ -86,26 +303,25 @@ void run::generateVM(string &vmType,string &vmCpuCores,string &vmMemory,string &
     else{
         _vmTwoNodes =false;
     }
-    VMWarehouse[_vmType] = new VM(_vmCpuCores,_vmMemory,_vmTwoNodes);
-    cout<<_vmCpuCores<<","<<_vmMemory<<","<<_vmTwoNodes<<endl;
+    VMWarehouse[_vmType] = new VM(_vmType, _vmCpuCores,_vmMemory,_vmTwoNodes);
 }
 
 
 // 解析用户添加请求
-void run::generateRequest(string &op, string &reqVmType, string &reqId){
+void run::generateRequest(string &op, string &reqVmType, string &reqId,int today){
     string _op,_reqVmType,_reqId;
     _op = op.substr(1,op.size() -1);
     _reqVmType = reqVmType.substr(0,reqVmType.size() -1);
     _reqId = reqId.substr(0,reqId.size() -1);
-    requestWarehouse.push_back(new request(true,_reqVmType,_reqId));
+    requestWarehouse[today].push_back(new request(true,_reqVmType,_reqId));
 }
 
 // 解析用户删除请求
-void run::generateRequest(string &op, string &reqId){
+void run::generateRequest(string &op, string &reqId, int today){
     string _op,_reqId;
     _reqId = reqId.substr(0,reqId.size() -1);
     _op = op.substr(1,op.size() -1);
-    requestWarehouse.push_back(new request(false,"",_reqId));
+    requestWarehouse[today].push_back(new request(false,"",_reqId));
 }
 
 
@@ -135,6 +351,7 @@ void run::loadData(){
     //读入所有的请求信息
     int RequestNumber = 0;
     scanf("%d",&days);
+    requestWarehouse=new vector<request*>[days];
     string op,reqVmType,reqId;
     for(int i=0;i<days;i++){
         scanf("%d",&RequestNumber);
@@ -142,10 +359,10 @@ void run::loadData(){
             cin>>op;
             if(op[1] == 'a'){
                 cin>>reqVmType>>reqId;
-                generateRequest(op, reqVmType, reqId);
+                generateRequest(op, reqVmType, reqId, i);
             }else{
                 cin>>reqId;
-                generateRequest(op, reqId);
+                generateRequest(op, reqId, i);
             }
         }
     }
